b3bbee71b9af9bd9589a057ec8ebe022cf4fc275
rezit1234 <rezit@atomicmail.io>
2026-07-20 10:47:30 +0200

    dashdhjasda3

--- src/components/ChatFab.tsx
+++ src/components/ChatFab.tsx
@@ -58,7 +58,7 @@ export function ChatFab() {
   return (
     <div
       ref={wrapRef}
-      className="fixed right-4 bottom-4 z-50 sm:right-6 sm:bottom-6"
+      className="fixed right-4 bottom-4 z-30 sm:right-6 sm:bottom-6"
     >
       <div
         id={panelId}
